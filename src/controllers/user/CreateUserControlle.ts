49ff00ed4d7c16bc9533801af5d5c61fed97fdfa
Gilson Mendes <gilson.mendes00@hotmail.com>
2023-03-27 17:06:19 -0300

    Add CreateUserService

--- src/controllers/user/CreateUserControlle.ts
+++ src/controllers/user/CreateUserControlle.ts
@@ -1,10 +1,16 @@
 import {Request, response, Response} from 'express';
-
+import { CreateUserService } from '../../services/user/CreateUserService';
 class CreateUserController{
    
     async handle(req: Request, res: Response) {
-        console.log(req.body)
-        return res.json({ok:true})
+        const {name, email, password} = req.body;
+        const createUserService = new CreateUserService();
+        const user = await createUserService.execute({
+            name, 
+            email, 
+            password
+        });
+        return res.json(user)
     }
 }
 
