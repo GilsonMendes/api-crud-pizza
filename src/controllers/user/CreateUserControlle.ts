b6b8d520244a0f0e725e227d3bc4bc01724e764c
Gilson Mendes <gilson.mendes00@hotmail.com>
2023-03-27 16:28:50 -0300

    Add pasta controller e arquivos

---
+++ src/controllers/user/CreateUserControlle.ts
@@ -0,0 +1,11 @@
+import {Request, response, Response} from 'express';
+
+class CreateUserController{
+   
+    async handle(req: Request, res: Response) {
+        console.log(req.body)
+        return res.json({ok:true})
+    }
+}
+
+export {CreateUserController};
