340ecfca24120b9ad8883f6b73c14b4a749f372d
Gilson Mendes <gilson.mendes00@hotmail.com>
2023-03-27 07:43:57 -0300

    Add libs do prisma io

--- src/routes.ts
+++ src/routes.ts
@@ -3,7 +3,7 @@ import { Router, Request, Response } from 'express';
 const router = Router();
 
 router.get('/teste', (req: Request, res: Response) => {
-    return res.json({ ok: true })
+    throw new Error('Erro ao fazer essa requisição!')
 })
 
 export { router }
