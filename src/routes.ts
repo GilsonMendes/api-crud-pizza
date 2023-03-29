a8146d63a4c1b85cd6615d7b04a77199913ff252
Gilson Mendes <gilson.mendes00@hotmail.com>
2023-03-29 10:57:26 -0300

    Add novas libs

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,10 @@
-import { Router} from 'express';
+import { Router } from 'express';
 import { CreateUserController } from './controllers/user/CreateUserControlle';
+import { AuthUserController } from './controllers/user/AuthUserController';
+
 const router = Router();
 
 router.post('/users', new CreateUserController().handle)
+router.post('/sesion', new AuthUserController().handle)
 
 export { router }
---
+++ src/services/user/AuthUserService.ts
@@ -0,0 +1,35 @@
+
+import prismaClient from '../../prisma';
+import { compare } from 'bcryptjs';
+
+interface AuthRequest {
+    email: string;
+    password: string;
+
+}
+class AuthUserService {
+    async execute({ email, password }: AuthRequest) {
+        const user = await prismaClient.user.findFirst({
+            where: {
+                email: email
+            }
+        })
+
+        if (!user) {
+         throw new Error("User/password not found")  
+         return;
+        }
+
+        const passwordMatch = await compare(password, user.password)
+
+        if (!passwordMatch) {
+            throw new Error("User/password not found")
+            return;
+        }
+        return { ok: true }
+
+    }
+
+}
+
+export { AuthUserService };
