a4776124ef1c6ec3673f7bb303f9bf7becbc067a
Gilson Mendes <gilson.mendes00@hotmail.com>
2023-03-29 21:36:05 -0300

    Add Types jsonwebtoken

--- src/services/user/AuthUserService.ts
+++ src/services/user/AuthUserService.ts
@@ -1,37 +1,30 @@
-
-import prismaClient from '../../prisma';
-import { compare } from 'bcryptjs';
+import prismaClient from "../../prisma";
+import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
 
 interface AuthRequest {
-    email: string;
-    password: string;
-
+  email: string;
+  password: string;
 }
 class AuthUserService {
-    async execute({ email, password }: AuthRequest) {
-        const user = await prismaClient.user.findFirst({
-            where: {
-                email: email
-            }
-        })
+  async execute({ email, password }: AuthRequest) {
+    const user = await prismaClient.user.findFirst({
+      where: {
+        email: email,
+      },
+    });
 
-        if (!user) {
-
-         throw new Error("User/password not found") 
-         
-        }
-
-        const passwordMatch = await compare(password, user.password)
-
-        if (!passwordMatch) {
-
-            throw new Error("User/password not found")
-            
-        }
-        return { ok: true }
-
+    if (!user) {
+      throw new Error("User/password not found");
     }
 
+    const passwordMatch = await compare(password, user.password);
+
+    if (!passwordMatch) {
+      throw new Error("User/password not found");
+    }
+    return { ok: true };
+  }
 }
 
 export { AuthUserService };
