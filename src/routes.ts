9cb063a6a66fb2f5d25965668740acd7babc3b91
Gilson Mendes <gilson.mendes00@hotmail.com>
2023-03-30 07:27:06 -0300

    Add get dos detalhes do usuário

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 import { CreateUserController } from './controllers/user/CreateUserControlle';
 import { AuthUserController } from './controllers/user/AuthUserController';
+import { DetailUseController } from './controllers/user/DetailUserController';
+
 
 const router = Router();
 
 router.post('/users', new CreateUserController().handle)
 router.post('/sesion', new AuthUserController().handle)
+router.get('/me',  new DetailUseController().handle)
 
 export { router }
