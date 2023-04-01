4d763baeea65d8dce40f60a7eb05eaf5b035ce7f
Gilson Mendes <gilson.mendes00@hotmail.com>
2023-03-31 22:14:33 -0300

    Add rotas

--- src/routes.ts
+++ src/routes.ts
@@ -4,7 +4,7 @@ import { AuthUserController } from "./controllers/user/AuthUserController";
 import { DetailUseController } from "./controllers/user/DetailUserController";
 import { isAuthenticated } from "./middleware/isAuthenticated";
 import { CreateCategotryController } from "./controllers/category/CreateCategotryController";
-
+import { ListCategoryController } from "./controllers/category/ListCategoryController";
 const router = Router();
 
 // Rotas de users
@@ -12,6 +12,10 @@ router.post("/users", new CreateUserController().handle);
 router.post("/sesion", new AuthUserController().handle);
 router.get("/me", isAuthenticated, new DetailUseController().handle);
 // Rotas de category
-router.post("/category", isAuthenticated, new CreateCategotryController().handle);
-
+router.post(
+  "/category",
+  isAuthenticated,
+  new CreateCategotryController().handle
+);
+router.get("/category", isAuthenticated, new ListCategoryController().handle);
 export { router };
